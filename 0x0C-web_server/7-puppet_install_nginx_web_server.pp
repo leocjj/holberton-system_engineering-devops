0a5411b0a9d8cb3df1ba3b6b1f83ddd335571a05
leocjj <leocj@hotmail.com>
2020-03-25 18:02:21 -0500

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,28 @@
 # Configuring a nginx server.	
 
-  # install nginx server
-  package { 'nginx':
-    ensure => installed,
-    name   => 'nginx',
-  }
-  
-  # create index.html
-  file { '/var/www/html/index.html':
-    path    => '/var/www/html/index.html',
-    content => 'Holberton School'
-  }
+# install nginx server
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
 
-  # Redirection configuration
-  file_line { 'redirection':
-    ensure   => present,
-    path     => '/etc/nginx/sites-available/default',
-    after    => 'server_name _;',
-    line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-    multiple => true
-  }
+# create index.html
+file { '/var/www/html/index.html':
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School'
+}
 
-  # start server to apply changes
-  service { 'nginx':
-    ensure  => running,
-    require => Package['nginx']
-  }
+# Redirection configuration
+file_line { 'redirection':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true
+}
+
+# start server to apply changes
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
