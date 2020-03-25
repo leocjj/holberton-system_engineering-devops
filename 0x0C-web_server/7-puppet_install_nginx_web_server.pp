fbc38c1992e7ebd0bd344f381bf8b1ea9ba90ee3
leocjj <leocj@hotmail.com>
2020-03-25 17:21:04 -0500

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,18 +7,16 @@
 
   # create index.html
   file { '/var/www/html/index.html':
-    ensure  => 'present',
+    path    => '/var/www/html/index.html'
     content => 'Holberton School'
   }
-
-# sudo ufw allow 'Nginx HTTP' ?
 
   # Redirection configuration
   file_line { 'title':
     ensure   => present,
     path     => '/etc/nginx/sites-available/default',
     after    => 'listen 80 default_server;',
-    line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
+    line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
     multiple => true
   }
 
