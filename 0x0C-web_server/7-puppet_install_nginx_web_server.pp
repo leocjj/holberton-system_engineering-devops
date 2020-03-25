879b887ef88324e1db0ec032760cb1b7224c6c8e
leocjj <leocj@hotmail.com>
2020-03-25 17:15:13 -0500

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,14 +1,8 @@
 # Configuring a nginx server.	
 
-  # executes apt-get update
-  exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
-  }
-
   # install nginx server
   package { 'nginx':
     ensure   => 'installed'
-    name   => 'nginx'
   }
 
   # create index.html
