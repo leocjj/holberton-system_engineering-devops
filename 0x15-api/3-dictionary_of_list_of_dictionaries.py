9155513e84f6a019c98fde7bff4c092ce42f2631
leocjj <leocj@hotmail.com>
2020-05-04 21:12:30 -0500

    Create 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API
+(https://jsonplaceholder.typicode.com/), records all tasks from all
+employees, returns information about his/her TODO list progress and to
+export data in the JSON format.
+"""
+
+if __name__ == '__main__':
+    ids = set()
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    data = r.json()
+    for user in data:
+        ids.add(user.get('userId'))
+
+    output = {}
+    for user in ids:
+        r = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                         format(user))
+        username = r.json().get('username')
+
+        r = requests.get('https://jsonplaceholder.typicode.com/' +
+                         'todos?userId={}'.format(user))
+        data = r.json()
+
+        output['{}'.format(user)] = []
+        for task in data:
+            output['{}'.format(user)].append({
+                'task': task.get('title'),
+                'completed': task.get('completed'),
+                'username': username
+            })
+
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump({int(x): output[x] for x in output.keys()},
+                  file, sort_keys=True)
