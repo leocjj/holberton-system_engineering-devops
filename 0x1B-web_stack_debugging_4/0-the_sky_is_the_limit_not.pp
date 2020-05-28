c96902e1864a2354d2066f9f127c083a8b8931b3
leocjj <leocj@hotmail.com>
2020-05-28 17:33:58 -0500

    Create 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Change ULIMIT from 15 to 4096
+exec { 'limit':
+  command => "sed -i -e 's/15/4096/g' /etc/default/nginx; service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
